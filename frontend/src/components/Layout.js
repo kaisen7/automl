0cddc04604e819c8ec1e2bd164d7a61cbd87ed86
kaisen7 <charanponaganti7@gmail.com>
2026-04-03 10:10:15 +0530

    small change :)

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -293,7 +293,7 @@ export default function Layout({ children }) {
               <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"/>
             </svg>
           </div>
-          <span className="nav-wordmark-text">Auto<span>ML</span></span>
+          <span className="nav-wordmark-text">Auto<span>ML  :)</span></span>
         </div>
 
         {/* Desktop links */}
